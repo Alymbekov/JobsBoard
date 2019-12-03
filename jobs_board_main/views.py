2fdfe0405e8781a8d818a96c62350600aab7dedd
Alymbekov <maxim.makarov.1997@mail.ru>
2019-12-03 14:38:52 +0600

    add app conf

--- jobs_board_main/views.py
+++ jobs_board_main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from jobs_board_main.models import Job
+from jobs_board_main.models import Job, Subscriber, Subscription
 
 
 def get_jobs(request):
@@ -8,3 +8,22 @@ def get_jobs(request):
     jobs = Job.objects.all()
     return render(request, 'jobs_board_main/jobs.html', {'jobs': jobs})
 
+
+def get_job(request, id):
+   job = Job.objects.get(pk=id)
+   return render(request, 'jobs_board_main/job.html', {'job': job})
+
+
+def subscribe(request, id):
+    job = Job.objects.get(pk=id)
+    sub = Subscriber(email=request.POST['email'])
+    sub.save()
+
+    subscription = Subscription(user=sub, job=job)
+    subscription.save()
+
+    payload = {
+        'job': job,
+        'email': request.POST['email']
+    }
+    return render(request, 'jobs_board_main/subscribed.html', {'payload': payload})
